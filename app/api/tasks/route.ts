a9cc985acbfe7173954209ee71a44bd17bb06a3d
masudrana-1 <masudrana58855@gmail.com>
2023-12-11 18:57:21 +0600

    create error in api

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,13 +1,38 @@
+import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 
 // create 
 export async function POST(req: Request) {
     try {
+
+        const { userId } = auth();
+
+        if (!userId) {
+            return NextResponse.json({ error: "Unauthorized", status: 401 })
+        }
+
+        const { title, description, date, isCompleted, isImportant } = await req.json();
+        if (!title || !description || !date) {
+            return NextResponse.json({
+                error: "Missing required fields",
+                status: 400,
+            });
+        }
+
+        if (title.length > 3) {
+            return NextResponse.json({
+                error: "Title must be at least 3 characters long",
+                status: 400,
+            });
+        }
+
+        
+
         
     } catch (error) {
         console.log("ERROR CREATING TASKS:", error);
-        return NextResponse.json({ error: "Error creating task", status: 500 });
+        return NextResponse.json({ error: "Error creating task", status: 400 });
     }
 }
 
@@ -17,7 +42,7 @@ export async function GET(req: Request) {
         
     } catch (error) {
         console.log("ERROR GETTING TASKS:", error);
-        return NextResponse.json({ error: "Error getting task", status: 500 });
+        return NextResponse.json({ error: "Error getting task", status: 400 });
     }
 }
 
@@ -27,7 +52,7 @@ export async function PUT(req: Request) {
         
     } catch (error) {
         console.log("ERROR UPDATING TASKS:", error);
-        return NextResponse.json({ error: "Error updating task", status: 500 });
+        return NextResponse.json({ error: "Error updating task", status: 400 });
     }
 }
 
@@ -37,6 +62,6 @@ export async function DELETE(req: Request) {
         
     } catch (error) {
         console.log("ERROR DELETING TASKS:", error);
-        return NextResponse.json({ error: "Error deleting task", status: 500 });
+        return NextResponse.json({ error: "Error deleting task", status: 400 });
     }
 }
