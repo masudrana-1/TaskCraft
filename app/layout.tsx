163e55d0dfe97e315cecf75f796e87c295fbd372
masudrana-1 <masudrana58855@gmail.com>
2023-12-13 18:44:12 +0600

    implement toploader

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Sidebar from './Components/Sidebar/Sidebar'
 import GlobalStylesProvider from './providers/GlobalStylesProvider'
 import ContextProvider from './providers/ContextProvider'
 import { ClerkProvider, auth } from '@clerk/nextjs'
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <NextTopLoader
+            height={2}
+            color='#27AE60'
+            easing='cubic-bezier(0.53, 0.21, 0, 1)'
+          />
           <ContextProvider>
             <GlobalStylesProvider>
               {userId && <Sidebar/>}
