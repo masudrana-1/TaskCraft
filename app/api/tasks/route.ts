e832bb2a67005d5cc23d8d33e445fffc853eaaa4
masudrana-1 <masudrana58855@gmail.com>
2023-12-16 15:41:41 +0600

    create put route

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -81,7 +81,28 @@ export async function GET(req: Request) {
 // Update 
 export async function PUT(req: Request) {
     try {
-        
+        const { userId } = auth();
+        const { isCompleted, id } = await req.json();
+
+        if (!userId) {
+            return NextResponse.json({ error: "Unauthorized", status: 401 })
+        }
+
+        const task = await prisma.task.update({
+            where: {
+                id,
+            },
+            data: {
+                isCompleted,
+            }
+        })
+
+        console.log("Task Updated:", task);
+
+
+        return NextResponse.json(task)
+
+
     } catch (error) {
         console.log("ERROR UPDATING TASKS:", error);
         return NextResponse.json({ error: "Error updating task", status: 400 });
