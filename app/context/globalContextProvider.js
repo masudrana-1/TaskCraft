89406ab163466664362f8c96de7362e4b3278d08
masudrana-1 <masudrana58855@gmail.com>
2023-12-18 16:48:00 +0600

    sort by time

--- app/context/globalContextProvider.js
+++ app/context/globalContextProvider.js
@@ -40,7 +40,17 @@ export const GlobalProvider = ({ children }) => {
         try {
             const res = await axios.get("api/tasks")
 
-            setTasks(res.data);
+
+            // sort by time 
+            const sorted = res.data.sort((a, b) => {
+                return (
+                    new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+                );
+            });
+
+            console.log(sorted)
+
+            setTasks(sorted);
             setIsLoading(false)
         } catch (error) {
             console.log(error);
