2fde339cf1647f7f01e4b3427fd56ae4b25aaab6
Sukanta Das <arnobh84@gmail.com>
2023-12-21 18:14:40 +0600

    custom verify email page added

---
+++ app/signup/verify-email-address/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useSearchParams, redirect } from "next/navigation";
+import React, { useEffect } from "react";
+
+const page = () => {
+  const searchParams = useSearchParams();
+  const search = searchParams.get("redirect_url");
+  console.log(search);
+
+  useEffect(() => {
+    if (search) {
+      redirect(search);
+    }
+  }, [search]);
+
+  return <div>hello</div>;
+};
+
+export default page;
